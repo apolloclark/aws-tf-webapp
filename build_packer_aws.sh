6544ff73058df2e5ae4aeeea20960b200ab45a1c
apolloclark <apolloclark@gmail.com>
2018-02-01 12:17:07 -0500

    divided packer build into virtualbox and aws builds

--- build_packer_aws.sh
+++ build_packer_aws.sh
@@ -31,11 +31,11 @@ export SSH_PRIVATE_KEY_FILE="$HOME/.ssh/packer.pem"
 
 ### build Packer AMI
 
-packer validate packer.json
+packer validate packer_aws.json
 
-packer inspect packer.json
+packer inspect packer_aws.json
 
-packer build -only=amazon-ebs packer.json
+packer build -only=amazon-ebs packer_aws.json
 
 # print AMI ID
 export WEBAPP_AMI_ID=$(jq '.builds[-1].artifact_id' -r manifest.json | cut -d':' -f2);
