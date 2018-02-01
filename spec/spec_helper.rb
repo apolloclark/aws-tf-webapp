41e5836f306823a22612e8e234e21b764911f0b7
apolloclark <apolloclark@gmail.com>
2018-02-01 13:34:51 -0500

    removed Elastic beat Serverspec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -164,25 +164,11 @@ end
 
 # Define packages
 Packages = {
-  'filebeat' => {
-    version: '5.6.5'
-  },
-  'metricbeat' => {
-    version: '5.6.5'
-  },
-  'heartbeat' => {
-    version: '5.6.5'
-  },
-  'packetbeat' => {
-    version: '5.6.5'
-  },
-  'osquery' => {
-    version: '2.10.2-1.linux'
-  }
 }
 
 # Define enabled services
 EnabledServices = [
+  'mysql',
   'apache2'
 ]
 
